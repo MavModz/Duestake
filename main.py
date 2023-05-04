c9808548c30dbb664c89e4c659fcb939da15ee37
MavModz <gauravchauhan10082@gmail.com>
2023-05-04 10:10:33 +0530

    Saas added

--- main.py
+++ main.py
@@ -12,5 +12,9 @@ cursor = conn.cursor()
 def home():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('testing.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
